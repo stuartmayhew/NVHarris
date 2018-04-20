1f7744701f68eb4f9e1e36d1796d3dceb9800d05
stuartmayhew <stumay111@gmail.com>
2018-04-20 11:56:44 -0500

    initial

---
+++ NVHarris2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NVHarris2.MvcApplication" Language="C#" %>
---
+++ NVHarris2/Models/ComboBoxItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NVHarris2.Models
+{
+    public class ComboBoxItem
+    {
+        public string key { get; set; }
+        public int value { get; set; }
+        public string strValue { get; set; }
+    }
+}
